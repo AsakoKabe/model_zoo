ef6aa5d8f6d4000176d1f15bc121093f8b852036
Denis Mamatin <mamatin-denis@yandex.ru>
2022-05-07 19:24:44 +0700

    update urls

--- model_zoo/model_zoo/urls.py
+++ model_zoo/model_zoo/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from model_zoo import settings
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('zoo.urls'))
 ]
 
 if settings.DEBUG:
--- model_zoo/zoo/views.py
+++ model_zoo/zoo/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
 
-# Create your views here.
+from zoo.models import ModelCV
+from . import models
+
+
+class ZooHome(ListView):
+    model = ModelCV
+    template_name = 'zoo/index.html'
+    context_object_name = 'models'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Model Zoo'
+        return context
+
+    def get_queryset(self):
+        return models.ModelCV.objects.order_by('name')
